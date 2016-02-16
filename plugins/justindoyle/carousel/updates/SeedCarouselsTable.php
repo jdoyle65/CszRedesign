ab45ca7a068db5732531109d1dfb3fa4ee477996
Justin Doyle <jdoyle65@uwo.ca>
2016-02-15 20:37:20 -0500

    Finishing touched on carousel

--- plugins/justindoyle/carousel/updates/SeedCarouselsTable.php
+++ plugins/justindoyle/carousel/updates/SeedCarouselsTable.php
@@ -11,43 +11,20 @@ class SeedCarouselsTable extends Seeder {
         ]);
 
         CarouselImage::create([
-            'image_url' => 'cszlogo.png',
-            'title_en' => 'Hello',
-            'title_fr' => 'Bonjour',
-            'description_en' => 'That is good.',
-            'description_fr' => 'Ca c\'est bon.',
+            'image_url' => '/csz-chick.jpg',
+            'title_en' => 'Lorem ipsum dolor sit amet',
+            'title_fr' => 'Lorem ipsum dolor sit amet Francais',
+            'description_en' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a urna erat. Maecenas posuere mattis arcu. Sed ac sapien eleifend, pretium diam vitae, commodo erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque ut neque nec tellus consectetur malesuada. Aenean quis tellus purus. Nullam quis quam et mi iaculis maximus quis in arcu.',
+            'description_fr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a urna erat. Maecenas posuere mattis arcu. Sed ac sapien eleifend, pretium diam vitae, commodo erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque ut neque nec tellus consectetur malesuada. Aenean quis tellus purus. Nullam quis quam et mi iaculis maximus quis in arcu.',
             'link_out' => '#',
             'carousel_id' => $carousel->id
         ]);
         CarouselImage::create([
-            'image_url' => 'cszlogo.png',
-            'title_en' => 'Two',
-            'title_fr' => 'Deux',
-            'description_en' => 'How are you?',
-            'description_fr' => 'Comment ca va?',
-            'link_out' => '#',
-            'carousel_id' => $carousel->id
-        ]);
-
-        $carousel = Carousel::create([
-            'name' => 'Test Carousel 2'
-        ]);
-
-        CarouselImage::create([
-            'image_url' => 'cszlogo.png',
-            'title_en' => 'Hmmm',
-            'title_fr' => 'Hmmm',
-            'description_en' => 'That is good.',
-            'description_fr' => 'Ca c\'est bon.',
-            'link_out' => '#',
-            'carousel_id' => $carousel->id
-        ]);
-        CarouselImage::create([
-            'image_url' => 'cszlogo.png',
-            'title_en' => 'Yes',
-            'title_fr' => 'Oui',
-            'description_en' => 'How are you?',
-            'description_fr' => 'Comment ca va?',
+            'image_url' => '/csz-pig.jpeg',
+            'title_en' => 'Lorem ipsum dolor sit amet Two',
+            'title_fr' => 'Lorem ipsum dolor sit amet Deux',
+            'description_en' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a urna erat. Maecenas posuere mattis arcu. Sed ac sapien eleifend, pretium diam vitae, commodo erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque ut neque nec tellus consectetur malesuada. Aenean quis tellus purus. Nullam quis quam et mi iaculis maximus quis in arcu.',
+            'description_fr' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis a urna erat. Maecenas posuere mattis arcu. Sed ac sapien eleifend, pretium diam vitae, commodo erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque ut neque nec tellus consectetur malesuada. Aenean quis tellus purus. Nullam quis quam et mi iaculis maximus quis in arcu.',
             'link_out' => '#',
             'carousel_id' => $carousel->id
         ]);
