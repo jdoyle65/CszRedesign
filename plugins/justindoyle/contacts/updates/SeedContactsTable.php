d7c6c0988b27709c0d62de00adc2781f1bc960c8
Justin Doyle <jdoyle65@uwo.ca>
2016-03-06 23:15:09 -0500

    Update contacts seeders

--- plugins/justindoyle/contacts/updates/SeedContactsTable.php
+++ plugins/justindoyle/contacts/updates/SeedContactsTable.php
@@ -7,22 +7,87 @@ use JustinDoyle\Contacts\Models\Category;
 class SeedContactsTable extends Seeder {
 
     public function run() {
-        $category = Category::create([
+        $executive = Category::create([
             'title_en' => 'Executive',
-            'title_fr' => 'Executive'
+            'title_fr' => 'Exécutif'
+        ]);
+        $councilors = Category::create([
+            'title_en' => 'Councillors',
+            'title_fr' => 'Conseillers'
+        ]);
+        $studentCouncilors = Category::create([
+            'title_en' => 'Student Councillors',
+            'title_fr' => 'Conseillers étudiants'
         ]);
 
-        $contact = Contact::create([
-            'title_en' => 'President',
-            'title_fr' => 'Président',
-            'category_id' => $category->id,
-            'name' => 'Jane Doe',
-            'address_line_one' => '555 Fake St',
-            'city' => 'London',
-            'province' => 'ON',
-            'email' => 'jane@example.com',
-            'work_phone' => '(905) 555-1234',
+        Contact::create([
+            'title_en' => 'Past President',
+            'title_fr' => 'Président antérieur',
+            'category_id' => $executive->id,
+            'name' => 'Suzie Currie',
+            'address_line_one' => 'Mount Allison Univ., Biology',
+            'address_line_two' => '63B York St',
+            'city' => 'Sackville',
+            'province' => 'NB',
+            'postal_code' => 'E4L 3G7',
+            'email' => 'scurrie@mta.ca',
+            'work_phone' => '506-364-2260',
             'profile_image' => 'profile_female.jpg',
         ]);
+        Contact::create([
+            'title_en' => 'President',
+            'title_fr' => 'Président',
+            'category_id' => $executive->id,
+            'name' => 'James Staples',
+            'address_line_one' => 'Western Univ., Biology',
+            'address_line_two' => '1151 Richmond St',
+            'city' => 'London',
+            'province' => 'ON',
+            'postal_code' => 'N6A 5B8',
+            'email' => 'jfstaple@uwo.ca',
+            'work_phone' => '519-661-4057',
+            'profile_image' => 'profile_male.jpg',
+        ]);
+
+        Contact::create([
+            'title_en' => 'Secretary',
+            'title_fr' => 'Secrétaire',
+            'category_id' => $executive->id,
+            'name' => 'Helga Guderley',
+            'address_line_one' => 'Université Laval, Département de biologie',
+            'email' => 'helga.guderley@bio.ulaval.ca',
+            'work_phone' => '902-820-2979',
+            'profile_image' => 'profile_female.jpg',
+        ]);
+        Contact::create([
+            'title_en' => 'Councillor Retiring May 2016',
+            'title_fr' => 'Conseiller dont le mandat se terminera en mai 2016',
+            'category_id' => $studentCouncilors->id,
+            'name' => 'Laura Ferguson',
+            'address_line_one' => 'Western University',
+            'address_line_two' => '1151 Richmond St',
+            'city' => 'London',
+            'province' => 'ON',
+            'postal_code' => 'N6A 5B8',
+            'email' => 'lfergus9@uwo.ca',
+            'work_phone' => '902-799-9726',
+            'profile_image' => 'profile_female.jpg',
+        ]);
+        Contact::create([
+            'title_en' => 'Councillor Retiring May 2017',
+            'title_fr' => 'Conseiller dont le mandat se terminera en mai 2017',
+            'category_id' => $studentCouncilors->id,
+            'name' => 'Dillon Chung',
+            'address_line_one' => 'Univ. British Columbia, Zoology',
+            'address_line_two' => '6270 University Blvd',
+            'city' => 'Vancouver',
+            'province' => 'BC',
+            'postal_code' => 'V6T 1Z4',
+            'email' => 'dchungch@zoology.ubc.ca',
+            'work_phone' => '604-779-7754',
+            'profile_image' => 'profile_male.jpg',
+        ]);
+
+
     }
 }
